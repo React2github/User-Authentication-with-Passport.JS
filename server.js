21efcdb517b7904a2bf36a62e336f62a1ef2e9fc
Abdella <abdella.shukur2@gmail.com>
2020-05-11 12:00:39 -0400

    logout

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config')
 initializePassport(
@@ -27,26 +28,27 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get('/', (req,res) => {
+app.get('/', checkAuthenticated, (req,res) => {
     res.render('index.ejs', { name: req.user.name })
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
 }))
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated,(req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register',checkNotAuthenticated, async (req, res) => {
     try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     users.push({
@@ -61,7 +63,25 @@ app.post('/register', async (req, res) => {
     }
 })
 
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
 
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+       return res.redirect('/')
+    }
+    next()
+}
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
