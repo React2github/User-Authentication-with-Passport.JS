878bcd6429fe4694499a1ad881377e8601799614
Abdella <abdella.shukur2@gmail.com>
2020-05-11 03:32:45 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,66 @@
+if(process.env.Node_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+const initializePassport = require('./passport-config.js')
+initializePassport(
+    passport,
+     email => users.find(user => user.email === email)
+)
+const users = []
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
 });
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    })
+    res.redirect('/login')
+    } catch {
+     res.redirect('/register')
+    }
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
